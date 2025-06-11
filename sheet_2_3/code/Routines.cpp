70c9981731c046e31f5514a0d92d7c0d1c1b67b1
Niklas Bliemel <niklas.bliemel@stud.uni-regensburg.de>
2025-06-11 14:37:55 +0200

    test

--- sheet_2_3/code/Routines.cpp
+++ sheet_2_3/code/Routines.cpp
@@ -1,5 +1,7 @@
-#include "routines.h"
+#include "Routines.h"
 #include <vector>
+
+using namespace std;
 
 double sqr(double x)
 {
@@ -32,7 +34,33 @@ double vec_std(vector<double> &vec, int offset)
     return sqrt(vec_var(vec, offset));
 }
 
+double auto_covariance(vector<double> &vec, int t)
+{
+    int N = vec.size();
+    double y_minus = 0;
+    double y_plus = 0;
+    for (size_t i = 0; i < N - t; i++)
+    {
+        y_minus += vec[i];
+        y_plus += vec[i + t]
+    }
+    y_minus /= N - t;
+    y_plus /= N - t;
+
+    double out = 0;
+    for (size_t i = 0; i < N - t; i++)
+    {
+        out += (vec[i] - y_plus) * (vec[i + t] - y_minus);
+    }
+    return out;
+}
+
+double auto_correlation(vector<double> &vec, double t)
+{
+    return auto_covariance(t) / auto_covariance(0);
+}
+
 double auto_correlation_time(vector<double> &vec) // TODO
 {
-    return 0;
+    double out = 0.5;
 }
--- sheet_2_3/code/Routines.h
+++ sheet_2_3/code/Routines.h
@@ -11,6 +11,10 @@ double vec_var(vector<double> &vec, int offset);
 
 double vec_std(vector<double> &vec, int offset);
 
+double auto_covariance(vector<double> &vec);
+
+double auto_correlation(vector<double> &vec, double t);
+
 double auto_correlation_time(vector<double> &vec);
 
 #endif
